33cc9f59d3af1ddd9dfe50d233274585924d5727
Max Lopez <soytanmax10@gmail.com>
2021-09-14 11:09:26 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>Home component</h1>
+    <div class="error" v-if="error">{{ error }}</div>
+    <div class="" v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>loading...</div>
+    
+  </div>
+</template>
+
+<script>
+import getPosts from '../composables/getPosts'
+import PostList from '../components/PostList.vue'
+export default {
+  name: 'Home',
+  components: {
+    PostList
+  },
+  setup(){
+    const { posts, error, load } = getPosts()
+    load()
+
+    return {posts, error}
+  }
+}
+</script>
